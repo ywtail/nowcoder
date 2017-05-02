15f3fe757485565bfabeb2d1b6ba8bd19c427517
ywtail <ywtail@gmail.com>
2017-05-02 20:41:07 +0800

    17.3.19 棋子翻转

--- 2.py
+++ 2.py
@@ -38,7 +38,6 @@ print flip.flipChess1([[0,0,1,1],[1,0,1,0],[0,1,1,0],[0,0,1,0]],[[2,2],[3,3],[4,
 
 '''
 # 棋子翻转
-
 [题目链接](https://www.nowcoder.com/practice/0b5ab6cc51804dd59f9988ad70d8c4a0?tpId=49&tqId=29282&tPage=1&rp=1&ru=/ta/2016test&qru=/ta/2016test/question-ranking)
 
 **题目描述**
